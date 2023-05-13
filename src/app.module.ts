b4ad7ee5bec00c92fef4f8f167e851a3747edba8
Raghvendra Khatri <raghvendrakhatri@Raghvendras-MacBook-Pro.local>
2023-05-13 17:15:17 +0530

    feat: add interceptor to log req-res

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [{
+    provide: APP_INTERCEPTOR,
+    useClass: LoggingInterceptor,
+  },AppService],
 })
 export class AppModule {}
