43287de4e8b88bd82c9a452fcec72f8c447fc93b
Raghvendra Khatri <raghvendrakhatri@Raghvendras-MacBook-Pro.local>
2023-05-14 17:03:16 +0530

    feat: add datbase config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,30 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { APP_FILTER } from '@nestjs/core';
 import { AllExceptionsFilter } from './filters/allException.filter';
 import { TransformationInterceptor } from './interceptors/responseTransaform.interceptor';
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { config } from './modules/config/config';
+import { TypeOrmModule,TypeOrmModuleOptions } from '@nestjs/typeorm'
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal:true,
     load:[config]
-  })],
+  }),
+  TypeOrmModule.forRootAsync({
+    imports:[ConfigModule],
+    inject:[ConfigService],
+    useFactory:(configService:ConfigService):TypeOrmModuleOptions => ({
+      type:'postgres',
+      host:configService.get<string>('database.host'),
+      port:configService.get<number>('databse.port'),
+      username:configService.get<string>('database.username'),
+      password:configService.get<string>('database.password'),
+      database:configService.get<string>('databse.type'),
+      entities:[__dirname + '/../**/*.entity.{js,ts}'],
+      synchronize:false
+    })
+  })
+],
   controllers: [AppController],
   providers: [
     {
